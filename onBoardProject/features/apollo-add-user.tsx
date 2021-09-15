6d889efa13f8ac419b4fb89ca83108c0a8e6d7e8
MaTMolina <Matheus.Molina@taqtile.com.br>
2021-09-15 16:24:40 -0300

    CREATE_USER_MUTATION

---
+++ onBoardProject/features/apollo-add-user.tsx
@@ -0,0 +1,27 @@
+import {gql} from '@apollo/client';
+
+const CREATE_USER_MUTATION = gql`
+  mutation CreateUser(
+    $name: String!
+    $email: String!
+    $phone: String!
+    $birthDate: Date!
+    $password: String!
+    $role: UserRole!
+  ) {
+    createUser(
+      data: {
+        name: $name
+        email: $email
+        phone: $phone
+        birthDate: $birthDate
+        password: $password
+        role: $role
+      }
+    ) {
+      name
+      email
+      id
+    }
+  }
+`;
